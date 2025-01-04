86282b17109ba39292ea5468e8c8b9004ac3df23
manoj sharma <manoj.sharma.webdev@gmail.com>
2025-01-04 06:38:18 +0000

    addded nativepaper and notes

--- app/index.jsx
+++ app/index.jsx
@@ -1,14 +1,21 @@
+import { Link } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+
+import * as React from 'react';
+import { ActivityIndicator, MD2Colors } from 'react-native-paper';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>URl: /</Text>
+      <Text>This file in app/index.js</Text>
+      <Link href="/bills"> <Text>Jump to bills</Text></Link>
+      <Link href="/bills"> <Text>Jump to bills</Text></Link> 
+      <ActivityIndicator animating={true} color={MD2Colors.red800} />
       <StatusBar style="auto" />
     </View>
   );
 }
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
