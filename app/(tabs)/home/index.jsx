eb4bd342695d955ab1351788df4a86b9bac69422
manoj sharma <manoj.sharma.webdev@gmail.com>
2025-01-05 17:05:19 +0000

    build changes

--- app/(tabs)/home/index.jsx
+++ app/(tabs)/home/index.jsx
@@ -1,7 +1,7 @@
 import { View, BackHandler } from 'react-native'
 import { Linking } from 'react-native';
 import * as React from 'react';
-import { TextInput, Button } from 'react-native-paper';
+import { TextInput, Button, Snackbar } from 'react-native-paper';
 import { useEffect, useState } from 'react'
 import { useRouter } from 'expo-router'
 import { useTheme } from 'react-native-paper';
@@ -17,6 +17,18 @@ const Home = () => {
         }
     }
 
+    const [snackVisible, setSnackVisible] = useState(false);
+    const [snackText, setSnackText] = useState('');
+
+    const showSnack = (text) => {
+        setSnackText(text);
+        setSnackVisible(true);
+    };
+
+    const onDismissSnackBar = () => {
+        setSnackVisible(false);
+    };
+
     useEffect(() => {
         const handleBackPress = () => {
             BackHandler.exitApp(); // Exit the app directly
@@ -29,6 +41,7 @@ const Home = () => {
             BackHandler.removeEventListener('hardwareBackPress', handleBackPress);
         };
     }, []);
+
     const [number, setNumber] = useState("");
     const [isValid, setIsValid] = useState(false);
 
@@ -36,19 +49,53 @@ const Home = () => {
 
     const handleChatNow = async () => {
         console.log(number)
+
+        showSnack(`Number: ${number}`);
+        // console.log("testing with google")
+        // const s = await Linking.canOpenURL("https://google.com");
+        // if (s){
+        //     await Linking.openURL("https://google.com");
+        // }
+        // console.log("testing with google")
         url = `https://api.whatsapp.com/send/?phone=%2B91${number}&text&type=phone_number&app_absent=0`
+        showSnack(`URL: ${url}`);
         const supported = await Linking.canOpenURL(url);
+        showSnack(`Supported: ${supported}`);
         if (supported) {
             try {
                 await storeDataWithTimestamp('@recent-chat-data', { number: number });
+                showSnack(`Data saved`);
                 // Update UI or state after successful storage
             } catch (error) {
                 console.error("Error storing data:", error);
+                showSnack(`Error saving data: ${error.message}`);
             }
+            Snack(`Linking Whatsapp`)
             await Linking.openURL(url);
+            showSnack(`WhatsApp opened`);
         } else {
             console.warn(`Don't know how to open URI: ${url}`);
+            showSnack(`Error opening WhatsApp: ${error.message}`);
         }
+
+        return (
+            <View>
+                {/* Snackbar */}
+                <Snackbar
+                    visible={snackVisible}
+                    onDismiss={onDismissSnackBar}
+                    duration={2000}
+                    action={{
+                        label: 'Undo',
+                        onPress: () => {
+                            // Handle undo action
+                        },
+                    }}
+                >
+                    {snackText}
+                </Snackbar>
+            </View>
+        );
     }
 
     const handleNumberChange = (num) => {
@@ -62,6 +109,22 @@ const Home = () => {
     };
 
     const Br = () => <View style={{ height: 10 }} />;
+    const Snack = (text) => <View><Snackbar
+        visible={true}
+        onDismiss={onDismissSnackBar}
+        duration={500}
+        elevation={5}
+        action={{
+            label: 'Undo',
+            onPress: () => {
+                // Do something
+            },
+        }}>
+        {text}
+    </Snackbar>
+    </View>
+
+
     return (
         <View className="flex-1 justify-center items-center p-4 bg-white">
             <TextInput
